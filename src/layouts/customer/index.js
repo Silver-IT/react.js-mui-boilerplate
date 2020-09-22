721afd3fba92a362840f2547a52228a7bbf33a55
silver-it <silver.19920220@gmail.com>
2020-09-22 15:58:35 +0900

    feat: integreate redux for authentication ( just a simple routering )

--- src/layouts/customer/index.js
+++ src/layouts/customer/index.js
@@ -1,22 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 import { CustomerHomePage, CustomerProfilePage } from "../../pages/customer";
 
-const CustomerLayout = ({ match }) =>
-<div>
-    <h2>Customer Layout Header</h2> 
-    
-    <Switch>
-        <Route exact path={`${match.path}home`} component={CustomerHomePage} />
-        <Route exact path={`${match.path}profile`} component={CustomerProfilePage} />
+const CustomerLayout = ({ match }) => {
+    const isAuthenticated = useSelector(state => !!state.auth.user);
 
-        <Route path="">
-            <Redirect to="/404" />
-        </Route>
-    </Switch>
+    if (!isAuthenticated) {
+        return <Redirect to="/auth/login" />
+    }
     
-    <h2>Customer Layout Footer</h2> 
-</div>;
+    return <div>
+        <h2>Customer Layout Header</h2> 
+        
+        <Switch>
+            <Route exact path={`${match.path}home`} component={CustomerHomePage} />
+            <Route exact path={`${match.path}profile`} component={CustomerProfilePage} />
+
+            <Route path="">
+                <Redirect to="/404" />
+            </Route>
+        </Switch>
+        
+        <h2>Customer Layout Footer</h2> 
+    </div>
+};
 
 export default CustomerLayout;
--- src/pages/customer/home/index.js
+++ src/pages/customer/home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Button from "@material-ui/core/Button";
 
 const CustomerHomePage = () => {
+  const user = useSelector(state => state.auth.user);
+
   return (
     <Button variant="contained" color="primary">
-      Customer Home Page
+      Customer Home Page. Email is {user.email}
     </Button>
   );
 }
